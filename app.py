f138df89f19e31f81f32322e3152543bb14d8f78
Dhananjay Vikram Singh <singhdhananjay0920@gmail.com>
2025-12-17 13:48:22 +0530

    Machine learning project-1

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+#define functions and routes
+def get_cleaned_data(form_data):
+    gestation = float(form_data['gestation'])
+    parity = int(form_data['parity'])
+    age = float(form_data['age'])
+    height = float(form_data['height'])
+    weight = float(form_data['weight'])
+    smoke = float(form_data['smoke'])
+
+    cleaned_data = {
+        'gestation':[gestation],
+        'parity':[parity],
+        'age':[age],
+        'height':[height],
+        'weight':[weight],
+        'smoke':[smoke]
+        }
+
+    return cleaned_data
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods = ['POST'])
+def get_prediction():
+    #get data from user
+    baby_data_form = request.form
+    baby_data_cleaned = get_cleaned_data(baby_data_form)
+
+    #convert to dataframe
+    baby_df = pd.DataFrame(baby_data_cleaned)
+
+    #model calling
+    with open('model/model.pkl', 'rb') as obj:
+        model = pickle.load(obj)
+
+    #make prediction
+    prediction = model.predict(baby_df)
+    prediction = round(float(prediction), 2)
+
+    #return the result
+    response = {"prediction": prediction}
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
